ab918a601f972b0203cb7294f8c473c6f7ef3d92
Marcus Weinberger <marcus@marcusweinberger.com>
2020-07-08 19:39:47 +0000

    0.1.0

--- ruqqus/__init__.py
+++ ruqqus/__init__.py
@@ -1,4 +1,4 @@
-import requests, warnings, atoma
+import requests, warnings, atoma, urllib.parse
 
 class InvalidLogin(BaseException):
     pass
@@ -55,9 +55,33 @@ class RuqqusAPI(object):
     
     def user_listing(self, username):
         return self.handle(self.session.get(f"{self.base}/user/{username}/listing"))
+    
+    def home(self, only=None, sort="hot", t="all", page=1):
+        return self.session.get(f"{self.base}/front/listing?sort={sort}&t={t}&page={page}" + ("" if not only else f"&only={only}")).json()
+    
+    def front_all(self, page=1, sort="hot", t="all"):
+        return self.session.get(f"{self.base}/all/listing?page={page}&sort={sort}&t={t}").json()
     # </unofficial v1>
 
     # <unofficial>
+    def get_post_title(self, url):
+        return self.session.get("https://ruqqus.com/api/submit/title?url=" + urllib.parse.quote_plus(url)).json()
+
+    def giphy(self, searchTerm="", limit=""):
+        return self.session.get(f"https://ruqqus.com/giphy?searchTerm={searchTerm}&limit={limit}").json()
+    
+    def me(self):
+        return self.session.get("https://ruqqus.com/me").url
+
+    def random_post(self):
+        return self.session.get("https://ruqqus.com/random/post").url
+    
+    def random_guild(self):
+        return self.session.get("https://ruqqus.com/random/guild").url
+    
+    def random_user(self):
+        return self.session.get("https://ruqqus.com/random/user").url
+
     def feeds_public(self, sort="hot"):
         r = self.session.get("https://ruqqus.com/feeds/" + sort)
         try:
@@ -102,4 +126,65 @@ class RuqqusAPI(object):
     
     def vote_comment(self, comment_id, x="1"):
         return self.session.post("https://ruqqus.com/api/vote/comment/%s/%s" % (comment_id, x)).status_code == 204
+    
+    def admin_image_posts_listing(self, page=1):
+        return self.session.get("https://ruqqus.com/admin/image_posts?page=" + str(page)).json()
+    
+    def admin_ban_user(self, user_id, days=0, reason="", message=""):
+        return not self.session.post("https://ruqqus.com/api/ban_user/" + user_id, data={
+            "days": days,
+            "reason": reason,
+            "message": message
+        }).status_code == 400
+    
+    def admin_unban_user(self, user_id, alts=False):
+        return not self.session.post("https://ruqqus.com/api/unban_user/" + user_id, data={
+            "alts": alts
+        }).status_code == 400
+    
+    def admin_ban_post(self, post_id, reason=None):
+        return not self.session.post("https://ruqqus.com/api/ban_post/" + post_id, data={
+            "reason": reason
+        }).status_code == 400
+    
+    def admin_unban_post(self, post_id):
+        return not self.session.post("https://ruqqus.com/api/unban_post/" + post_id).status_code == 400
+    
+    def admin_distinguish_post(self, post_id):
+        return not self.session.post("https://ruqqus.com/api/distinguish/" + post_id).status_code in [404, 403]
+    
+    def admin_sticky_post(self, post_id):
+        return self.session.post("https://ruqqus.com/api/sticky/" + post_id).url
+    
+    def admin_ban_comment(self, cid):
+        return self.session.post("https://ruqqus.com/api/ban_comment/" + cid).status_code == 204
+    
+    def admin_unban_comment(self, cid):
+        return self.session.post("https://ruqqus.com/api/unban_comment/" + cid).status_code == 204
+    
+    def admin_distinguish_comment(self, cid):
+        return self.session.post("https://ruqqus.com/api/distinguish_comment/" + cid).status_code == 204
+    
+    def admin_undistinguish_comment(self, cid):
+        return self.session.post("https://ruqqus.com/api/undistinguish_comment/" + cid).status_code == 204
+    
+    def admin_ban_guild(self, bid, reason=""):
+        return self.session.post("https://ruqqus.com/api/ban_guild/" + bid, data={
+            "reason": reason
+        }).url
+    
+    def admin_unban_guild(self, bid):
+        return self.session.post("https://ruqqus.com/api/unban_guild/" + bid).url
+    
+    def admin_mod_self(self, bid):
+        return self.session.post("https://ruqqus.com/api/mod_self/" + bid).url
+    
+    def user_stat_data(self, days=30):
+        return self.session.get("https://ruqqus.com/api/user_stat_data?days=" + str(days)).json()
+    
+    def admin_csam_nuke(self, pid):
+        return self.session.post("https://ruqqus.com/admin/csam_nuke/" + pid)
+    
+    def guild_profile(self, guild):
+        return self.session.get("https://ruqqus.com/+%s/pic/profile" % guild).url
     # </unofficial>
