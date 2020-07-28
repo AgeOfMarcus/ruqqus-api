18b712de55be58f1ed188a6ee7b427ec34410e8e
Marcus Weinberger <marcus@marcusweinberger.com>
2020-07-28 17:16:21 +0000

    update

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ def read(fname):
     return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
 setuptools.setup(name="ruqqus-api",
-    version="0.3.6",
+    version="0.4.0",
     description="Unofficial Ruqqus API Wrapper",
     long_description=read("README.md"),
     long_description_content_type="text/markdown",
